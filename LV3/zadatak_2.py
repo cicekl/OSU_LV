0860a922389e848e61233c90453141c9a55e9089
Lorena <cicek.lorena@gmail.com>
2025-03-27 19:34:25 +0100

    Treće vježbe riješene.

---
+++ LV3/zadatak_2.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv('data_C02_emission.csv')
+
+
+# a) Pomocu histograma prikažite emisiju C02 plinova. Komentirajte dobiveni prikaz.
+plt.figure()
+data['CO2 Emissions (g/km)'].plot(kind='hist', bins=20)
+plt.show()
+
+# b) Pomocu dijagrama raspršenja prikažite odnos izmedu gradske potrošnje goriva i emisije 
+# C02 plinova. Komentirajte dobiveni prikaz. Kako biste bolje razumjeli odnose izmedu
+# velicina, obojite tockice na dijagramu raspršenja s obzirom na tip goriva
+
+plt.figure()
+data['Fuel Code'] = data['Fuel Type'].astype('category').cat.codes
+data.plot.scatter(x='Fuel Consumption City (L/100km)',
+                  y='CO2 Emissions (g/km)',
+                  c='Fuel Code', cmap ="hot", s=50)
+plt.show()
+
+# c) Pomocu kutijastog dijagrama prikažite razdiobu izvangradske potrošnje s obzirom na tip 
+# goriva. Primjecujete li grubu mjernu pogrešku u podacima?
+
+plt.figure()
+data.boxplot(column=['Fuel Consumption Hwy (L/100km)'], by='Fuel Type')
+plt.show()
+
+# d) Pomocu stupcastog dijagrama prikažite broj vozila po tipu goriva. Koristite metodu 
+# groupby.
+
+plt.figure()
+grouped = data.groupby('Fuel Type').size()
+grouped.plot(kind='bar', color='skyblue', edgecolor='black')
+plt.show()
+
+# e) Pomocu stupcastog grafa prikažite na istoj slici prosjecnu C02 emisiju vozila s obzirom na
+# broj cilindara.
+avgEmission = data.groupby('Cylinders')['CO2 Emissions (g/km)'].mean()
+avgEmission.plot(kind='bar',color='salmon', edgecolor='black')
+plt.show()
